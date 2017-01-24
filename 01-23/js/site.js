09aacb7ac38767ea10147c2b0bd92db307fee05a
Karl Stolley <karl.stolley@gmail.com>
2017-01-24 00:22:42 -0600

    Added jQuery 3 document-ready handler and jQuery 2 style in comment

--- 01-23/js/site.js
+++ 01-23/js/site.js
@@ -1,1 +1,17 @@
+$.noConflict();
 
+// jQuery 3-style scoped $ and
+// silent ready event
+jQuery(function($) {
+  $('h1').html('DOM Loaded!');
+});
+
+// jQuery 2-style scoped $ with
+// self-invoking function and
+// ready event
+//
+// (function($) {
+//   $(document).on('ready', function() {
+//     $('h1').html('DOM Loaded!');
+//   });
+// })(jQuery);
