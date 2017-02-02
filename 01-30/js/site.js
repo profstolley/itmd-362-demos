fbe7028ee7c61bb7d29518683cc1378aae0f2571
Karl Stolley <karl.stolley@gmail.com>
2017-02-01 18:25:42 -0600

    Added CSS and JS to show/hide Submit based on input

--- 01-30/js/site.js
+++ 01-30/js/site.js
@@ -5,4 +5,16 @@ $.noConflict();
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+
+  $('#email').on('keyup focus blur', function() {
+    var currentValue = $(this).val();
+    var validPattern = $(this).attr('pattern');
+    console.log(currentValue,validPattern);
+    if(currentValue.match(validPattern)) {
+      $('#submit').addClass('active');
+    } else {
+      $('#submit').removeClass('active');
+    }
+  });
+
 });
