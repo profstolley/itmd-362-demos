3b9a3d60a9e73e0e3ea0963303472d8772c27b91
Karl Stolley <karl.stolley@gmail.com>
2017-02-06 13:04:21 -0600

    Added files for week of February 6, including Moby Dick passage

---
+++ 02-06/js/site.js
@@ -0,0 +1,8 @@
+// Tell jQuery to give up the dollar sign
+$.noConflict();
+
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
