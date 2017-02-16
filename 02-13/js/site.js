ea22a9cfd7a48180870741422f37a200e87cf671
Karl Stolley <karl.stolley@gmail.com>
2017-02-15 18:33:19 -0600

    Added JavaScript and state styles for animated email label

--- 02-13/js/site.js
+++ 02-13/js/site.js
@@ -5,4 +5,12 @@ $.noConflict();
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $('#email').on('focus', function() {
+      $('#input-email label').addClass('active');
+  });
+  $('#email').on('blur', function() {
+    if($('#email').val().length == 0) {
+      $('#input-email label').removeClass('active');
+    }
+  });
 });
