6cc7bc5d392b7f923aa8c4293908894c016adf9e
Karl Stolley <karl.stolley@gmail.com>
2017-04-10 16:01:58 -0500

    WIP Toggler on show/hide password; clean up number of args

--- 04-10/js/site.js
+++ 04-10/js/site.js
@@ -3,16 +3,29 @@ $.noConflict();
 
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
+
+  function toggleLabel(oldLabel,newLabel,currentLabel) {
+    if (oldLabel === currentLabel) {
+      return newLabel;
+    }
+    else {
+      return oldLabel;
+    }
+  }
+
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
   // A little usability feature; let people show the password if privacy isn't
   // a big deal (for example, the user is alone)
-  $('#password-input').append('<a id="show-password" href="#null">Show Password</a>');
-  $('#show-password').on('click', function(e) {
-    $('#password').attr('type','text');
-    e.preventDefault();
+  $('#password-input').append('<a href="#null" id="password-toggle">Show Password</a>');
+  $('#password-toggle').on('click', function(e){
+    $('#password').attr('type',toggleLabel('password','text',$('#password').attr('type')));
+    $('#password-toggle').html(toggleLabel('Show Password','Hide Password', $('#password-toggle').html()));
   });
+
+
+
 
   // Mock up unsuccessful and successful login attempts; this demo requires
   // a username of 'tbernerslee' and will accept any password
